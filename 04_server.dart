d981bb780747c4aa6af942692a64193a7851100c
师振昆 <2936418868@qq.com>
2022-06-23 17:26:49 +0800

    新增

--- 04_server.dart
+++ 04_server.dart
@@ -10,7 +10,7 @@ import 'package:shelf_router/shelf_router.dart' as shelf_router;
 import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
 Future<void> main(List<String> args) async {
-  // 从环境变量里读取 PORT 变量用作端口，斗则用 '8080'
+  // 从环境变量里读取 PORT 变量用作端口，否则用 '8080'
   // ?? 快捷操作符（https://blog.csdn.net/nimeghbia/article/details/100921620）
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
